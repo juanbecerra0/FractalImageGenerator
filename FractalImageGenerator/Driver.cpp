c6117c57aa2d85ea53375db03ec4048620193565
juanbecerra0 <47235983+juanbecerra0@users.noreply.github.com>
2020-06-08 09:50:06 -0600

    Implemented color histogram

--- FractalImageGenerator/Driver.cpp
+++ FractalImageGenerator/Driver.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdint>
+#include <memory>
+#include <math.h>
 
 // Headers
 #include "Bitmap.h"
@@ -18,27 +20,50 @@ int main() {
 	double min = 999999;
 	double max = -999999;
 
+	unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS] { 0 } );
+	unique_ptr<int[]> fractal(new int[WIDTH * HEIGHT]{ 0 });
+
 	for (int y = 0; y < HEIGHT; y++) {
 		for (int x = 0; x < WIDTH; x++) {
-			double xFractal = (x - WIDTH / 2) * (2.0 / WIDTH);
+			double xFractal = (x - WIDTH / 2 - 200) * (2.0 / HEIGHT);
 			double yFractal = (y - HEIGHT / 2) * (2.0 / HEIGHT);
 
 			int iterations = Mandelbrot::getIterations(xFractal, yFractal);
 
-			uint8_t red = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
+			fractal[y * WIDTH + x] = iterations;
 
-			bitmap.setPixel(x, y, red, red, red);
-
-			if (xFractal < min) min = red; // xFractal;
-			if (xFractal > max) max = red; //xFractal;
-
-			cout << x << ", " << y << endl;
+			if(iterations != Mandelbrot::MAX_ITERATIONS)
+				histogram[iterations]++;
 		}
 	}
 
-	cout << min << ", " << max << endl;
+	int total = 0;
+	for (int i = 0; i < Mandelbrot::MAX_ITERATIONS; i++) {
+		total += histogram[i];
+	}
 
-	//bitmap.setPixel(WIDTH / 2, HEIGHT / 2, 255, 255, 255);
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+
+			uint8_t red = 0;
+			uint8_t green = 0;
+			uint8_t blue = 0;
+
+			int iterations = fractal[y * WIDTH + x];
+			if (iterations != Mandelbrot::MAX_ITERATIONS) {
+
+				double hue = 0.0;
+				for (int i = 0; i <= iterations; i++) {
+					hue += ((double)histogram[i] / total);
+				}
+
+				green = pow(255, hue);
+			}
+
+			bitmap.setPixel(x, y, red, green, blue);
+		}
+	}
+
 	bitmap.write("test.bmp");
 
 	cout << "Finished!" << endl;
